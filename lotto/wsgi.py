2b7efcd99febc8e6183eff1610b1b584660fa0df
JiHoon <eejjhh222@forbiz.co.kr>
2019-09-25 14:09:51 +0900

    초기 기본셋팅

---
+++ lotto/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for lotto project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+path = os.path.abspath(__file__+'/../..')
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lotto.settings')
+
+application = get_wsgi_application()
