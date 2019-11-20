f42eb6cdd0215ed0ad4592a76bb63f2ff8b236f0
JiHoon <eejjhh222@forbiz.co.kr>
2019-11-20 18:33:32 +0900

    lotto_data DB_table 생성

--- getCron/admin.py
+++ getCron/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from getCron.models import lotto_data
 
-# Register your models here.
+
+admin.site.register(lotto_data)
--- getCron/models.py
+++ getCron/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class lotto_data(models.Model):
+    round = models.IntegerField(default=0)
+    prize1 = models.IntegerField(default=0)
+    prize2 = models.IntegerField(default=0)
+    prize3 = models.IntegerField(default=0)
+    prize4 = models.IntegerField(default=0)
+    prize5 = models.IntegerField(default=0)
+    bonus = models.IntegerField(default=0)
+    drawing_day = models.DateTimeField()
+
