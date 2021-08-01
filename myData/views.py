be696dc2f5b46bfde63fe21b7ec965e3ca21c233
JiHoon <eejjhh222@naver.com>
2021-08-02 00:28:58 +0900

    데이터바인딩 & 통계그래프 이미지저장

--- myData/views.py
+++ myData/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
-import pandas as pd
-import seaborn as sns
+
 import matplotlib.pyplot as plt
 import warnings
+import numpy as np
+import seaborn as sns
+import pandas as pd
 
 
 # Create your views here.
@@ -18,9 +20,32 @@ def index(request):
 
 
 def static(request):
+    static_root = "static/image/"
     titanic = sns.load_dataset('titanic')
     em_pie = titanic['embarked'].value_counts()
-    em_pie.plot(kind='pie', autopct='%1.1f%%', figsize=(10,6)) # 원그래프
-    sns.countplot(data=titanic, x='embarked') # 막대그래프
+    em_pie.plot(kind='pie', autopct='%1.1f%%', figsize=(10, 6))  # 원그래프
+    sns.countplot(data=titanic, x='embarked')  # 막대그래프
 
-    return HttpResponse("Hello")
+    # join1 = sns.jointplot(x="age", y="fare", data=titanic)
+    # join1.savefig(static_root + "join1.png")
+    # join2 = sns.jointplot(x="age", y="fare", data=titanic, kind="kde")
+    # join2.savefig(static_root + "join2.png")
+    # plt.clf()
+    dataSet = [];
+    for data in titanic.itertuples():
+        dataSet.insert(data.Index, {
+            "survived": data.survived,
+            "sex": data.sex,
+            "age": data.age,
+            "sibsp": data.sibsp,
+            "embarked": data.embarked,
+            "who": data.who,
+            "alive": data.alive,
+            "alone": data.alone
+        })
+
+        if(data.Index > 10) :
+            break
+
+    context = {'staticData': dataSet}
+    return render(request, 'mydata/static_view.html', context)
